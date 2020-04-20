8772c12a422d70288394846edb82ef6613fd5236
Sergey Sokolov <dmemory7@ya.ru>
2020-04-20 22:03:29 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Yandex Metrika VuePress plugin
+
+## Install
+
+```shell
+yarn add vuepress-plugin-metrika
+```
+
+## Usage
+
+In your `.vuepress/config.js` add:
+
+```js
+module.exports = {
+    plugins: [
+        ...
+        ['metrika', {metrikaCounter: 'XXXXXXX'}]
+    ]
+}
+```
---
+++ enhanceAppFile.js
@@ -0,0 +1,18 @@
+export default ({ router }) => {
+    if (process.env.NODE_ENV === 'production' && GA_ID && typeof window !== 'undefined') {
+        (function (m, e, t, r, i, k, a) {
+            m[i] = m[i] || function() {
+                (m[i].a = m[i].a || []).push(arguments)
+            };
+            m[i].l = 1 * new Date();
+            k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a);
+        })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym")
+
+        ym(metrikaCounter, "init", {
+            clickmap:true,
+            trackLinks:true,
+            accurateTrackBounce:true,
+            webvisor:true
+        });
+    }
+}
